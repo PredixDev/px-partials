e65fac94aca500775a90d5d4f025884d5a1f17fe
Jonnie Spratley <jonnie.spratley@ge.com>
2016-06-22 14:53:08 -0700

    fix: updated all docs and all demos

--- test/px-partials-custom-tests.js
+++ test/px-partials-custom-tests.js
@@ -12,24 +12,73 @@ function runCustomTests() {
       test('renders', function () {
         assert.ok(element);
       });
+
+      test('it should fire px-header-link-select event', function (done) {
+        element.addEventListener('px-header-link-select', function (e) {
+          assert(e);
+          done();
+        });
+        element._handleTap();
+      });
+
+      test('it should fire px-header-user-select event', function (done) {
+        element.addEventListener('px-header-user-select', function (e) {
+          assert(e);
+          done();
+        });
+        element._handleUserTap();
+      });
     });
+
     suite('<px-footer>', function () {
       var element = document.getElementById('px_footer_1');
       test('renders', function () {
         assert.ok(element);
       });
     });
+
+    suite('<px-title-bar>', function () {
+      var element = document.getElementById('px_title_bar_1');
+      test('renders', function () {
+        assert.ok(element);
+      });
+    });
+
     suite('<px-button>', function () {
       var element = document.getElementById('px_button_1');
       test('renders', function () {
         assert.ok(element);
       });
+
+      test('it should fire px-button-tap event', function (done) {
+        element.addEventListener('px-button-tap', function (e) {
+          assert(e);
+          done();
+        });
+        element._clickHandler();
+      });
+
+      test('should update styles when modifier changes', function (done) {
+        element.modifier = 'primary large';
+        assert.ok(element.$.btn.classList.contains('btn--large'));
+        assert.ok(element.$.btn.classList.contains('btn--primary'));
+        done();
+      });
     });
+
     suite('<px-icon>', function () {
       var element = document.getElementById('px_icon_1');
       test('renders', function () {
         assert.ok(element);
       });
+
+      test('is should add the correct size and color to the icon', function (done) {
+        assert.ok(element);
+        assert(element.$.icn.style.fill === 'crimson');
+
+        done();
+      });
+
     });
     suite('<px-brand>', function () {
       var element = document.getElementById('px_brand_1');
--- wct.conf.js
+++ wct.conf.js
@@ -11,7 +11,7 @@ module.exports = {
       "dir": "./coverage",
       "reporters": ["text-summary", "lcov"],
       "include": [
-				"/px-*.html"
+				"/px-*/px-*.html"
 			],
       "exclude": []
     }
